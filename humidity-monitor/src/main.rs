255aa7302e5556daa6e6f992c2608f40a666275b
tomaisthorpe <tomaisthorpe@gmail.com>
2024-10-20 17:38:28 +0100

    feat: add connect to wifi

--- humidity-monitor/src/main.rs
+++ humidity-monitor/src/main.rs
@@ -1,16 +1,22 @@
 #![no_std]
 #![no_main]
 
-use core::str;
+use core::str::{self, FromStr};
+use cyw43::JoinOptions;
 use cyw43_pio::PioSpi;
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_net::{DhcpConfig, StackResources};
 use embassy_rp::bind_interrupts;
+use embassy_rp::clocks::RoscRng;
 use embassy_rp::gpio::{Level, Output};
 use embassy_rp::peripherals::{DMA_CH0, PIO0};
 use embassy_rp::pio::{InterruptHandler, Pio};
+use embassy_time::Timer;
+use rand::RngCore;
+use serde::Deserialize;
 use static_cell::StaticCell;
-use {defmt_rtt as _, panic_probe as _};
+use {defmt_rtt as _, panic_probe as _, serde_json_core};
 
 bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => InterruptHandler<PIO0>;
@@ -23,14 +29,34 @@ async fn cyw43_task(
     runner.run().await
 }
 
-// #[embassy_executor::task]
-// async fn net_task(mut runner: embassy_net::Runner<'static, cyw43::NetDriver<'static>>) -> ! {
-//     runner.run().await
-// }
+#[embassy_executor::task]
+async fn net_task(mut runner: embassy_net::Runner<'static, cyw43::NetDriver<'static>>) -> ! {
+    runner.run().await
+}
+
+#[derive(Deserialize)]
+struct Config<'a> {
+    ssid: &'a str,
+    password: &'a str,
+}
+
+fn parse_config() -> Option<Config<'static>> {
+    let config = include_bytes!("./config.json");
+    match serde_json_core::de::from_slice::<Config>(config) {
+        Ok((config, _used)) => Some(config),
+        Err(_) => {
+            error!("Failed to parse config");
+            None
+        }
+    }
+}
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     info!("Hello! Starting up...");
+
+    let config = unwrap!(parse_config());
+    info!("Connecting to: {}", config.ssid);
 
     let p = embassy_rp::init(Default::default());
     let fw = include_bytes!("../../cyw43-firmware/43439A0.bin");
@@ -51,7 +77,7 @@ async fn main(spawner: Spawner) {
 
     static STATE: StaticCell<cyw43::State> = StaticCell::new();
     let state = STATE.init(cyw43::State::new());
-    let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    let (net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
     unwrap!(spawner.spawn(cyw43_task(runner)));
 
     control.init(clm).await;
@@ -60,10 +86,52 @@ async fn main(spawner: Spawner) {
         .set_power_management(cyw43::PowerManagementMode::PowerSave)
         .await;
 
-    let mut scanner = control.scan(Default::default()).await;
-    while let Some(bss) = scanner.next().await {
-        if let Ok(ssid_str) = str::from_utf8(&bss.ssid) {
-            info!("SSID: {}", ssid_str);
+    // Connect to the network
+    let mut dhcp_config: DhcpConfig = Default::default();
+    dhcp_config.hostname = Some(heapless::String::from_str("humidity-monitor").unwrap());
+    let net_config = embassy_net::Config::dhcpv4(dhcp_config);
+
+    // Generate random seed
+    let mut rng = RoscRng;
+    let seed = rng.next_u64();
+
+    // Init network stack
+    static RESOURCES: StaticCell<StackResources<5>> = StaticCell::new();
+    let (stack, runner) = embassy_net::new(
+        net_device,
+        net_config,
+        RESOURCES.init(StackResources::new()),
+        seed,
+    );
+
+    unwrap!(spawner.spawn(net_task(runner)));
+
+    loop {
+        match control
+            .join(config.ssid, JoinOptions::new(config.password.as_bytes()))
+            .await
+        {
+            Ok(_) => break,
+            Err(err) => {
+                info!("join failed with status={}", err.status);
+            }
         }
     }
+
+    // Wait for DHCP, not necessary when using static IP
+    info!("waiting for DHCP...");
+    while !stack.is_config_up() {
+        Timer::after_millis(100).await;
+    }
+    info!("DHCP is now up!");
+
+    info!("waiting for link up...");
+    while !stack.is_link_up() {
+        Timer::after_millis(500).await;
+    }
+    info!("Link is up!");
+
+    info!("waiting for stack to be up...");
+    stack.wait_config_up().await;
+    info!("Stack is up!");
 }
